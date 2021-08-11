0a830e3735d9a4b6121e4ba5e1258f76f2d07bd8
Rafael Araujo <rafa.maraujo@hotmail.com>
2021-08-11 11:04:29 -0300

    Iniciando novo Projeto

--- NovoProjeto/Program.cs
+++ NovoProjeto/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
-namespace NovoProjeto {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Ola Mundo!");
-            Console.WriteLine("Deus te abençoe!");
+namespace NovoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
         }
     }
 }
