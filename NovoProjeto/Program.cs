a78fd4e280d9c8bd101bfbbb49f10ef88d7fd2ae
Rafael Araujo <rafa.maraujo@hotmail.com>
2021-08-11 09:52:03 -0300

    Adicionado Ola mundo

--- NovoProjeto/Program.cs
+++ NovoProjeto/Program.cs
@@ -4,6 +4,7 @@ namespace NovoProjeto {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola Mundo!");
         }
     }
 }
